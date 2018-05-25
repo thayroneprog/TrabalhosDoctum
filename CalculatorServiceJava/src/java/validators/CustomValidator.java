a55180eb9e040ce8ff3100c9dece20dfba8a8867
Thayrone Marques <thayroneprogsi@gmail.com>
2018-05-25 17:33:33 -0300

    JavaClient - Corrigindo uma linha no código do validators e tentando o build da aplicação

---
+++ CalculatorServiceJava/src/java/validators/CustomValidator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validators;
+
+import com.sun.xml.wss.impl.callback.PasswordValidationCallback;
+import com.sun.xml.wss.impl.callback.PasswordValidationCallback.PasswordValidator;
+import com.sun.xml.wss.impl.callback.PasswordValidationCallback.PlainTextPasswordRequest;
+
+
+
+public class CustomValidator implements PasswordValidator {
+
+    @Override
+    public boolean validate(PasswordValidationCallback.Request rqst) throws PasswordValidationCallback.PasswordValidationException {
+       PlainTextPasswordRequest req;
+        req = (PlainTextPasswordRequest)rqst;
+       return (req.getUsername().equals("user") && req.getPassword().equals("pwd"));
+    }
+
+    
+}
